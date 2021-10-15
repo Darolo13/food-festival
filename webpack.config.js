9810606fde2780c7968c7eb3ee05539e2c84a987
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-10-15 11:05:58 -0700

    Optimize Images with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,38 @@ const path = require('path');
 
 module.exports = {
     entry: {
-       app: "./assets/js/script.js",
-       events: "./assets/js/event.js",
-       schedule: "./assets/js/schedule.js",
-       tickets: "./assets/js/tickets.js"
+        app: "./assets/js/script.js",
+        events: "./assets/js/event.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
     },
     output: {
         filename: "[name].bundle.js",
         path: __dirname + "/dist",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name(file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function (url) {
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
